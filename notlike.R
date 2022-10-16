aa51107a14c4ce4ad6493dded9f5c07a767fdd52
Christian Kevin Kusuma <73664921+c-kevin-kusuma@users.noreply.github.com>
2022-10-16 00:25:36 -0600

    Delete notlike.R

--- notlike.R
+++
@@ -1,6 +0,0 @@
-#' The opposite of the DescTools '%like%' operator
-#'
-#' This function check for the opposite of the DescTools '%like%' operator.
-#' @export
-
-`%!like%` <- Negate(DescTools::`%like%`)
