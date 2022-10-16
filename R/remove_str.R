fbc99171ff7d9b616e61473fea2a99e8d8614eee
Christian Kevin Kusuma <73664921+c-kevin-kusuma@users.noreply.github.com>
2022-10-15 23:32:52 -0600

    Add files via upload

---
+++ R/remove_str.R
@@ -0,0 +1,6 @@
+#' Remove a certain value
+#'
+#' This function is a straight copy of the 'str_remove_all' function from 'stringr' package.
+#' @export
+
+remove_str <- stringr::str_remove_all
---
+++ R/replace_str.R
@@ -0,0 +1,6 @@
+#' Replace a certain value with an alternative
+#'
+#' This function is a straight copy of the 'str_replace' function from 'stringr' package.
+#' @export
+
+replace_str <- stringr::str_replace
